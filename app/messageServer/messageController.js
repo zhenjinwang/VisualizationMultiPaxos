729ff2449d4ffd309f1d7e38eef3207428ef28cd
zhenjin wang <zhenjin.wang@stonybrook.edu>
2015-12-11 22:41:09 -0500

    clean and format messageServer and messageController
    make the backend to be able to support multi-user if need and easier to
    read

--- app/messageServer/messageController.js
+++ app/messageServer/messageController.js
@@ -1,29 +1,154 @@
-module.exports=function(){
-     var exec = require('child_process').exec;
-     var paxos_driver='init';
-     var cmd = 'py -m da ../DistAlgoCompiler/distalgo/examples/vrpaxos/orig.da';
-     var fs = require('fs');
+module.exports = function() {
+    var exec = require('child_process').exec,
+        paxos_driver = 'init',
+        cmd = 'py -m da ../DistAlgoCompiler/distalgo/examples/vrpaxos/orig.da',
+        fs = require('fs');
 
-     this.saveData=function(data){
-       fs.writeFile('app/Data/paxosData.json',JSON.stringify(data),function(err){
-             console.log(err);
+    var messagesQueue = [],
+        servers = [{
+            'type': 'acceptor',
+            'ports': []
+        }, {
+            'type': 'leader',
+            'ports': []
+        }, {
+            'type': 'replica',
+            'ports': []
+        }, {
+            'type': 'client',
+            'ports': []
+        }],
+        mapping = {
+            'acceptor': 0,
+            'leader': 1,
+            'replica': 2,
+            'client': 3
+        },
+        startTime,
+        endTime,
+        count_done = 0,
+        replica = 3,
+        acceptor = 3,
+        leader = 1,
+        client = 1,
+        totalServers = 8,
+        totalCPUTime = 0,
+        totalElapseTime = 0,
+        status = false;
+    // start paxos driver to produce real time message log
+    this.startPaxosDriver = function(acceptor_num, leader_num, replica_num, client_num) {
+        // to avoid large number of paxos servers to cause crash
+        acceptor = (acceptor_num>0&&acceptor_num<6)?acceptor_num:3;
+        leader = (leader_num>0&&leader_num<6)?leader_num:1;
+        replica = (replica_num>0&&replica_num<6)?replica_num:3;
+        client = (client_num>0&&client_num<6)?client_num:1;
+        console.log(acceptor + ' ' + leader + ' ' + replica + ' ' + client);
+        totalServers = acceptor + leader + replica + client;
+        count_done = 0;
+        totalCPUTime = 0;
+        totalElapseTime = 0;
+        messagesQueue = [];
+        servers = [{
+            'type': 'acceptor',
+            'ports': []
+        }, {
+            'type': 'leader',
+            'ports': []
+        }, {
+            'type': 'replica',
+            'ports': []
+        }, {
+            'type': 'client',
+            'ports': []
+        }];
+        if (!status) // make sure at most one paxos is running
+            this.execPaxosDriver(acceptor, leader, replica, client);
+        status = true;
+    }
+    //reverse the status
+    this.changeStatus = function() {
+        status = !status;
+    }
+    //add message object to messagesQueue
+    this.addMessage = function(message) {
+        messagesQueue.push(message);
+        if (message.action == 'done') {
+            count_done++;
+            totalCPUTime += message.message[1];
+            totalElapseTime += message.message[2];
+            console.log('count done' + count_done);
+            if (count_done == totalServers) {
+                this.saveMessageLog({
+                    'data': messagesQueue,
+                    'servers': servers,
+                    'cpu_time': totalCPUTime / totalServers,
+                    'elapse_time': totalElapseTime / totalServers
+                });
+            }
+        } else {
+            if (message.action == 'start') {
+                servers[mapping[message.message[0]]].ports.push(message.from);
+            }
+        }
+    }
+    //return whether Master receives enough 'done' message from paxos driver
+    this.messageLogReady = function() {
+        return totalServers == count_done || (leader > 1 && totalServers / 2 < count_done);
+    }
+    // return a message log object
+    this.messageLog = function() {
+        console.log(totalServers + ' ' + count_done + ' ' + leader);
+        status=false;
+        this.killProcess();
+        return {
+            'data': messagesQueue,
+            'servers': servers,
+            'cpu_time': totalCPUTime / totalServers,
+            'elapse_time': totalElapseTime / totalServers
+        };
+    }
+    //return a empty message log object
+    this.emptyMessageLog = function() {
+        return {
+            'data': [],
+            'servers': {}
+        };
+    }
+    //save message log object to local storage--a json file
+    this.saveMessageLog = function(data) {
+        fs.writeFile('app/Data/paxosData.json', JSON.stringify(data), function(err) {
+            console.log(err);
         });
-     }
-     this.readData=function(){
-          return JSON.parse( fs.readFileSync("app/Data/paxosData.json"));
-     }
-     this.killProcess=function(){
-         if(paxos_driver!='init'){
-             paxos_driver.kill('SIGTERM');
-             console.log('kill child process');
-         }
-     }
-     this.execPaxosDriver=function(acceptor,leader,replica,client){
-       this.killProcess();// to make sure previous child process is killed
-       paxos_driver=exec('py -m da app/DistAlgoCompiler/distalgo-master/examples/vrpaxos/orig.da '+
-                        acceptor+' '+leader+' '+replica+' '+client,function(err,out,code){
-                                console.log('finish');
-                                return 'init';
-                    });
-     }
+    }
+    //read message log object from local storage
+    this.readMessageLog = function() {
+        try {
+            var content = JSON.parse(fs.readFileSync("app/Data/paxosData.json"));
+            return {
+                'data': content.data,
+                'servers': content.servers,
+                'cpu_time': content.cpu_time,
+                'elapse_time': content.elapse_time
+            };
+        } catch (e) {
+            return this.emptyMessageLog();
+        }
+    }
+    // kill the child process which runs paxos driver
+    this.killProcess = function() {
+        if (paxos_driver != 'init') {
+            paxos_driver.kill('SIGTERM');
+            console.log('kill child process');
+        }
+    }
+    // execute the paxos driver 
+    this.execPaxosDriver = function(acceptor, leader, replica, client) {
+        this.killProcess(); // to make sure previous child process is killed
+        paxos_driver = exec('py -m da app/DistAlgoCompiler/distalgo-master/examples/vrpaxos/orig.da ' +
+            acceptor + ' ' + leader + ' ' + replica + ' ' + client,
+            function(err, out, code) {
+                console.log('finish');
+                return 'init';
+            });
+    }
 }
--- app/messageServer/messageServer.js
+++ app/messageServer/messageServer.js
@@ -1,57 +1,31 @@
 'use strict';
-// initial value of paxos driver and messages
-var collect=[];
-var messagesQueue=[],
-    servers=[{'type':'acceptor','ports':[]},
-              {'type':'leader','ports':[]},
-              {'type':'replica','ports':[]},
-              {'type':'client','ports':[]}],
-    mapping={'acceptor':0,'leader':1,'replica':2,'client':3},
-    initialSetting,
-    startTime,
-    endTime,
-    count_done=0,
-    replica=3,
-    acceptor=3,
-    leader=1,
-    client=1,
-    totalServers=8,
-    totalCPUTime=0,
-    totalElapseTime=0,
-    status=false;
 
 // Net Socket to collect messages(buffer stream) from Paxos Driver
 var net = require('net');
 var server = net.createServer();
-var controller=require('../messageServer/messageController'),
-    messageController=new controller();
+var controller = require('../messageServer/messageController'),
+// each user has one messageController instance. It can be easily extended to support multi-user
+    messageController = new controller();
 
-server.on('connection',function(socket){
-  socket.on('data', function(data){
-    data=JSON.parse(data.toString('ascii'));
-    messagesQueue.push(data)
-    if(data.action=='done'){
-       count_done++;
-       totalCPUTime+=data.message[1],
-       totalElapseTime+=data.message[2];
-       if(count_done==totalServers){
-         messageController.saveData({'data':messagesQueue,'servers':servers,'cpu_time':totalCPUTime/totalServers,'elapse_time':totalElapseTime/totalServers});
-       }
-       console.log('count done'+count_done);
-    }else{
-      if(data.action=='start'){
-            servers[mapping[data.message[0]]].ports.push(data.from);
-      }
-    }
-  });
-  socket.on('error', function(data){
-    console.log(data);
-  });
+server.on('connection', function(socket) {
+    socket.on('data', function(data) {
+        try {
+            var message = JSON.parse(data.toString('ascii'));
+            messageController.addMessage(message);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+    socket.on('error', function(data) {
+        console.log(data);
+    });
 });
 
 server.listen(6000, function() { //'listening' listener
-  console.log('server bound');
+    console.log('server bound');
 });
+
+
 
 //Http server communicates with front end
 var express = require('express'),
@@ -59,48 +33,44 @@ var express = require('express'),
     app = express(),
     http = require('http').Server(app);
 
-
-var corsOptions=function(req,callback){
-  var corsoptions={origin:true,credentials:true};
-  callback(null,corsoptions);
+var corsOptions = function(req, callback) {
+    var corsoptions = {
+        origin: "http://localhost:9000"===req.headers.origin
+    };
+    callback(null, corsoptions);
 };
+
 app.use(cors(corsOptions)); // support cors domain request
 
 app.get('/api/data', function(req, res) {
-    if(totalServers==count_done||(leader>1&&totalServers/2<count_done)){
-      console.log(totalServers+' '+count_done+' '+leader);
-      status=false;
-      messageController.killProcess();
-      res.json({'data':messagesQueue,'servers':servers,'cpu_time':totalCPUTime/totalServers,'elapse_time':totalElapseTime/totalServers});     
-    }else{
-      res.json({'data':[],'servers':{}});
+    if (messageController.messageLogReady()) {
+        res.json(messageController.messageLog());
+    } else {
+        res.json(messageController.emptyMessageLog());
     }
 });
+
 app.get('/api/pre/data', function(req, res) {
-      var content =messageController.readData();
-      res.json({'data':content.data,'servers':content.servers,'cpu_time':content.cpu_time,'elapse_time':content.elapse_time});
+    res.json(messageController.readMessageLog());
 });
+
 app.get('/api/start/:acceptor/:leader/:replica/:client', function(req, res) {
-    
-    acceptor=parseInt(req.params.acceptor),
-    leader=parseInt(req.params.leader),
-    replica=parseInt(req.params.replica),
-    client=parseInt(req.params.client);
-    totalServers=acceptor+leader+replica+client;
-    count_done=0;
-    totalCPUTime=0,
-    totalElapseTime=0;
-    messagesQueue=[];
-    servers=[{'type':'acceptor','ports':[]},
-              {'type':'leader','ports':[]},
-              {'type':'replica','ports':[]},
-              {'type':'client','ports':[]}];
-    console.log(acceptor+' '+leader+' '+replica+' '+client);
-    console.log(count_done+' '+messagesQueue);
-    if(!status)// make sure at most one paxos is running
-        messageController.execPaxosDriver(acceptor,leader,replica,client);
-    status=true;
-    res.json({'ready':true});
+    try{
+      var acceptor = parseInt(req.params.acceptor),
+          leader = parseInt(req.params.leader),
+          replica = parseInt(req.params.replica),
+          client = parseInt(req.params.client);
+    }catch(error){
+          //default setting
+          acceptor=3;
+          leader=1;
+          replica=3;
+          client=1;
+    }
+    messageController.startPaxosDriver(acceptor, leader, replica, client);
+    res.json({
+        'ready': true
+    });
 });
 
 http.listen(5000);
